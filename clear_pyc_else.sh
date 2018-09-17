e458fabc6889570300060b3d718f80f86e05f2a7
lyqsr <81363943@example.com>
2018-09-17 12:55:41 +0800

    ...

--- clear_pyc_else.sh
+++ clear_pyc_else.sh
@@ -1,3 +1,14 @@
+echo "hidden folder or file st "
+find . -name ".*"
+echo "hidden folder or file ed "
+
+
+echo ""
+echo ""
+
+
 find . -name '*.pyc' -type f -print -exec rm -rf {} \;
 find . -name '*.py~' -type f -print -exec rm -rf {} \;
 find . -name '__pycache__' -type d -exec rm -rf {} \;
+
+
